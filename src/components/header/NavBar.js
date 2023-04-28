63b9f412d9a1c7a8f8d478fdf1261c087ffa2b3a
KelsGaming <alcorizamichael@gmail.com>
2023-04-28 16:07:57 +0800

    Fixing search bar size

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,20 +1,20 @@
 function NavBar() {
     return (
         <>
-            <div className="container-fluid" style={{background: "#1ed760"}}>
-                <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div className="container">
+                <header className="d-flex align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                     <div className="col-md-3 mb-2 mb-md-0">
                         <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-                            <h1 style={{color: "#191414"}}>Course Craft</h1>
+                            <h1>Course Craft</h1>
                             {/* <svg className="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap" /></svg> */}
                         </a>
                     </div>
-                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                    <form class="col-sm-5 col-lg mb-3 mb-lg-0 me-lg-3" role="search">
                         <input type="search" class="form-control form-control-secondary text-bg-secondary" placeholder="Search..." aria-label="Search" />
                     </form>
                     <div>
                         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                            <li><a href="#ID" className="nav-link px-2 link-secondary" style={{color: "#191414"}}>Dashboard</a></li>
+                            <li><a href="#ID" className="nav-link px-2 link-secondary">Dashboard</a></li>
                         </ul>
                     </div>
 
@@ -31,11 +31,11 @@ function NavBar() {
                         <button type="button" className="btn btn-primary">Sign-up</button>
                     </div> */}
                     <div class="dropdown text-end">
-                        <a href="#ID" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle" />
                         </a>
                         <ul class="dropdown-menu text-small">
-                            <li><a class="dropdown-item" href="#ID">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
                             <li><a class="dropdown-item" href="#">Settings</a></li>
                             <li><a class="dropdown-item" href="#">Profile</a></li>
                             {/* <li>< class="dropdown-divider"></li> */}
