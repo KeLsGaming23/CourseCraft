320eff21ce312e2df409c1315da452d6276d168e
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-04-28 18:05:57 +0800

    install react-router-dom

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -13,11 +13,11 @@ function NavBar() {
                     <form class="col-sm-5 col-lg mb-3 mb-lg-0 me-lg-3" role="search">
                         <input type="search" class="form-control form-control-secondary" placeholder="Search..." aria-label="Search" />
                     </form>
-                    <div>
+                    {/* <div>
                         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                             <li><Link to="/secondMain" className="nav-link px-2 link-secondary">Dashboard</Link></li>
                         </ul>
-                    </div>
+                    </div> */}
 
                     {/* <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                         <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
@@ -27,10 +27,10 @@ function NavBar() {
                         <li><a href="#" className="nav-link px-2">About</a></li>
                     </ul> */}
 
-                    {/* <div className="col-md-3 text-end">
+                    <div className="col-md-3 text-end">
                         <button type="button" className="btn btn-outline-primary me-2">Login</button>
                         <button type="button" className="btn btn-primary">Sign-up</button>
-                    </div> */}
+                    </div>
                     <div class="dropdown text-end">
                         <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle" />
