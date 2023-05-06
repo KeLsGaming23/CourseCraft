c08e6b4c106101412e871adb14865e6d4d5f7c33
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-05-06 14:41:22 +0800

    add a function for localstorage

--- src/context/CourseContext.js
+++ src/context/CourseContext.js
@@ -4,8 +4,13 @@ import { createContext, useState } from "react";
 export const CourseContext = createContext();
 export const CourseProvider = (props) => {
 
+    function getInitialState() {
+        const course = localStorage.getItem('course')
+        return course ? JSON.parse(course) : []
+    }
+
     const [courseData, setCourseData] = useState([]);
-    const [selectedCourse, setSelectedCourse] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState(getInitialState);
 
     return (
         <>
