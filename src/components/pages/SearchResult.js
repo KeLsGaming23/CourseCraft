3fcb56df8d7e1c0df7b8d1f2a455740e24feff24
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-05-10 13:52:29 +0800

    update search results

--- src/components/pages/SearchResult.js
+++ src/components/pages/SearchResult.js
@@ -14,20 +14,27 @@ function SearchResult() {
 
     return (
         <>
-            {result.length > 0 ? (
-                result.map(results => (
-                    <div className="card" key={results.id}>
-                        <img className="card-img-top" src="..." alt="" />
-                        <div className="card-body">
-                            <h5 className="card-title">{results.course_title}</h5>
-                            <p className="card-text">{results.course_description}</p>
-                            <button onClick={() => handleSelecteCourse(results)} className="btn btn-primary">Go somewhere</button>
+            <div className="container mt-5" style={{ height: "100vh" }}>
+                {result.length > 0 ? (
+                    result.map(results => (
+                        <div className="card mb-3" key={results.id} >
+                            <img className="card-img-top" src="..." alt="" />
+                            <div className="card-body">
+                                <h5 className="card-title">{results.course_title}</h5>
+                                <p className="card-text">{results.course_description}</p>
+                                <button onClick={() => handleSelecteCourse(results)} className="btn btn-primary">Explore Course</button>
+                            </div>
                         </div>
+                    ))
+
+                ) : (
+                    <div className="text-center mt-5">
+                        <h3>No results found.</h3>
+                        <p>Try to search other course</p>
                     </div>
-                ))
-            ) : (
-                <p>No results found.</p>
-            )}
+
+                )}
+            </div>
         </>
     );
 }
